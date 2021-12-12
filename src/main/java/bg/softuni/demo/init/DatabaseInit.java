d87923e87698dfecd40fe75f65b9df045ff10a19
George.P <georgipetroov@gmail.com>
2021-12-12 16:36:55 +0200

    Created database init

--- src/main/java/bg/softuni/demo/init/DatabaseInit.java
+++ src/main/java/bg/softuni/demo/init/DatabaseInit.java
@@ -1,4 +1,29 @@
 package bg.softuni.demo.init;
 
-public class DatabaseInit {
+import bg.softuni.demo.dao.RoleRepo;
+import bg.softuni.demo.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DatabaseInit implements CommandLineRunner {
+    private final RoleRepo roleRepo;
+
+    @Autowired
+    public DatabaseInit(RoleRepo roleRepo) {
+        this.roleRepo = roleRepo;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role roleAdmin = new Role();
+        roleAdmin.setName("ADMIN");
+        Role roleUser = new Role();
+        roleUser.setName("USER");
+
+        roleRepo.saveAll(List.of(roleAdmin, roleUser));
+    }
 }
