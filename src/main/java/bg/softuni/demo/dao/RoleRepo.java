5f9733fec3a9a388d0551d6ae8f620747d038ca8
George.P <georgipetroov@gmail.com>
2021-12-12 16:36:28 +0200

    Created role repo

--- src/main/java/bg/softuni/demo/dao/RoleRepo.java
+++ src/main/java/bg/softuni/demo/dao/RoleRepo.java
@@ -1,4 +1,9 @@
 package bg.softuni.demo.dao;
 
-public interface RoleRepo {
+import bg.softuni.demo.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepo extends JpaRepository<Role, String> {
 }
