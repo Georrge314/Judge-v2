ee8c099a7633cfa1ede1cd63e1db6f365944cc27
George.P <georgipetroov@gmail.com>
2021-12-14 14:34:32 +0200

    Added user service

--- src/main/java/bg/softuni/demo/service/UserService.java
+++ src/main/java/bg/softuni/demo/service/UserService.java
@@ -1,4 +1,13 @@
 package bg.softuni.demo.service;
 
+import bg.softuni.demo.model.service.UserRegisterService;
+
 public interface UserService {
+    void create(UserRegisterService userRegisterService);
+
+    boolean findByUsername(String username);
+
+    boolean findByEmail(String email);
+
+    boolean findByUsernameAndPassword(String username, String password);
 }
--- src/main/java/bg/softuni/demo/service/UserServiceImpl.java
+++ src/main/java/bg/softuni/demo/service/UserServiceImpl.java
@@ -1,4 +1,55 @@
 package bg.softuni.demo.service;
 
-public class UserServiceImpl {
+import bg.softuni.demo.dao.RoleRepo;
+import bg.softuni.demo.dao.UserRepo;
+import bg.softuni.demo.model.entity.Role;
+import bg.softuni.demo.model.entity.User;
+import bg.softuni.demo.model.service.UserRegisterService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final ModelMapper modelMapper;
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Autowired
+    public UserServiceImpl(ModelMapper modelMapper, UserRepo userRepo, RoleRepo roleRepo) {
+        this.modelMapper = modelMapper;
+        this.userRepo = userRepo;
+        this.roleRepo = roleRepo;
+    }
+
+    @Override
+    @Transactional
+    public void create(UserRegisterService userRegisterService) {
+        User user = modelMapper.map(userRegisterService, User.class);
+        Optional<Role> role = roleRepo.findByName("USER");
+        if (role.isPresent()) {
+            user.setRole(role.get());
+        } else {
+            user.setRole(new Role("USER"));
+        }
+        userRepo.save(user);
+    }
+
+    @Override
+    public boolean findByUsername(String username) {
+        return userRepo.findByUsername(username).isPresent();
+    }
+
+    @Override
+    public boolean findByEmail(String email) {
+        return userRepo.findByEmail(email).isPresent();
+    }
+
+    @Override
+    public boolean findByUsernameAndPassword(String username, String password) {
+        return userRepo.findByUsernameAndPassword(username, password).isPresent();
+    }
 }
