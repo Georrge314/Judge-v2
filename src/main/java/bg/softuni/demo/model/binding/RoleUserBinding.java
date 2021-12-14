92a097aa91e8f1618cc153cb872a5ed272c97990
George.P <georgipetroov@gmail.com>
2021-12-14 19:02:09 +0200

    created roleUserBinding dto

--- src/main/java/bg/softuni/demo/model/binding/RoleUserBinding.java
+++ src/main/java/bg/softuni/demo/model/binding/RoleUserBinding.java
@@ -1,4 +1,17 @@
 package bg.softuni.demo.model.binding;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
 public class RoleUserBinding {
+    @NotBlank
+    @NotNull
+    private String username;
+
+    @NotBlank
+    @NotNull
+    private String role;
 }
