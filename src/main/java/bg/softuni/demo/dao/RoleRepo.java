cae1e754513cfb60783666afa77655f0411ac3ad
George.P <georgipetroov@gmail.com>
2021-12-14 14:32:11 +0200

    Create User and Role repos

--- src/main/java/bg/softuni/demo/dao/RoleRepo.java
+++ src/main/java/bg/softuni/demo/dao/RoleRepo.java
@@ -1,9 +1,12 @@
 package bg.softuni.demo.dao;
 
-import bg.softuni.demo.model.Role;
+import bg.softuni.demo.model.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepo extends JpaRepository<Role, String> {
+    Optional<Role> findByName(String name);
 }
--- src/main/java/bg/softuni/demo/dao/UserRepo.java
+++ src/main/java/bg/softuni/demo/dao/UserRepo.java
@@ -1,4 +1,16 @@
 package bg.softuni.demo.dao;
 
-public interface UserRepo {
+import bg.softuni.demo.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
